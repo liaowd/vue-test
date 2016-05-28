d9c6da27dd753a5cb39355ae691de3ffbbfdbbb3
lwdmengda <winder7@yeah.net>
2016-05-28 23:56:32 +0800

    可以实现从M到V的数据绑定

--- js/index.js
+++ js/index.js
@@ -4,3 +4,21 @@ var vm=new Vue({
     message: 'Hello Vue.js!'
   }
 })
+
+vm.message='jjfie';
+
+var applePrice=new Vue({
+	el:'#apple',              //View
+	data:{                    //Model
+		p13:6888,
+		p15:9888,
+		dazhe:function(){
+			console.log(this.p13)
+			this.p13*=0.8;
+			this.p15*=0.8;
+		}		
+	},
+
+})
+
+applePrice.dazhe()
